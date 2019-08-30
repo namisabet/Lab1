6737ed8d89862682bdebd05facb3bc92ee266e2a
nami sabet <nami.sabet@hotmail.com>
2019-08-30 13:58:10 -0400

    Add project files.

---
+++ Lab1/Controllers/InfoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        public ActionResult Semester()
+        {
+            return View();
+        }
+        public ActionResult Testimonial()
+        {
+            return View();
+        }
+    }
+}
